60d77b151faa99d6a805d9ff6c34cf6c693382b4
Marcelo Marchetto <marcelomarchetto@MacBook-Pro-de-Marcelo.local>
2022-02-02 13:41:37 -0300

    draft js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Editor } from "react-draft-wysiwyg";
+import { convertFromRaw, convertToRaw, EditorState } from "draft-js";
+import draftToHtml from "draftjs-to-html";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
-function App() {
+export default function App() {
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
+  useEffect(() => {}, [editorState]);
+
+  const htm = () => {
+    const rawContentState = convertToRaw(editorState.getCurrentContent());
+    const markup = draftToHtml(rawContentState);
+    return {
+      __html: markup,
+    };
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React Editors</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <div className="p-4 border border-black m-4">
+        <Editor
+          editorClassName="bg-gray-100 mt-2 p-2"
+          placeholder="Digite algo..."
+          editorState={editorState}
+          onEditorStateChange={setEditorState}
+          toolbar={{
+            options: [
+              "inline",
+              "blockType",
+              "fontSize",
+              "fontFamily",
+              "textAlign",
+              "colorPicker",
+              "link",
+              "embedded",
+              "emoji",
+              "image",
+              "remove",
+              "history",
+            ],
+          }}
+        />
+      </div>
+
+      <div
+        dangerouslySetInnerHTML={htm()}
+        className="m-4 border border-black p-4"
+      ></div>
     </div>
   );
 }
-
-export default App;
